23ce9344498914b099d171ad4afe0222407c7f1c
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-02 18:47:06 +0100

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """db_storage"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.state import State
 from models.city import City
@@ -19,23 +19,17 @@ class DBStorage:
     """class  dbstorage"""
     __engine = None
     __session = None
-    classes = ['Amenity',
-               'City',
-               'User',
-               'Review',
-               'State',
-               'Place']
 
     def __init__(self):
         """ __init__(self) """
         self.__engine = create_engine(
-                'mysql+mysqldb://{}:{}@{}/{}'
-                .format(
-                        getenv('HBNB_MYSQL_USER'),
-                        getenv('HBNB_MYSQL_PWD'),
-                        getenv('HBNB_MYSQL_HOST'),
-                        getenv('HBNB_MYSQL_DB')),
-                pool_pre_ping=True)
+            'mysql+mysqldb://{}:{}@{}/{}'
+            .format(
+                getenv('HBNB_MYSQL_USER'),
+                getenv('HBNB_MYSQL_PWD'),
+                getenv('HBNB_MYSQL_HOST'),
+                getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(bind=self.__engine)
 
@@ -46,16 +40,15 @@ class DBStorage:
             for inst in self.__session.query(cls).all():
                 key = inst.__class__.__name__ + '.' + inst.id
                 dect[key] = inst
-            
+
         else:
             for clas in classes:
                 clas = eval(clas)
                 for inst in self.__session.query(clas).all():
                     key = inst.__class__.__name__ + '.' + inst.id
                     dect[key] = inst
-        return dect 
+        return dect
 
-    
     def save(self):
         """ commit  """
         self.__session.commit()
@@ -67,16 +60,16 @@ class DBStorage:
 
     def new(self, obj):
         """ add """
-        self.__session.add(obj) 
+        self.__session.add(obj)
 
     def reload(self):
         """create reload"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """public method"""
-        self.__session.remove()
-        
+        """Close the session."""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,6 @@ class FileStorage:
             return d
         return self.__objects
 
-
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
@@ -42,25 +41,21 @@ class FileStorage:
     def reload(self):
         """Loads storage dictionary from file"""
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-    
+
     def delete(self, obj=None):
-        """deletes an object from the dict __objects if it's inside"""
+        """method for deserializing the JSON file to objects"""
         if obj:
             del self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)]
             self.save()
-    
-    def close(self):
-        """public method"""
-        self.reload()
