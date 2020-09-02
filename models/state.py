f50f9cfc8ca27fb34967aa80f2377e8022959d94
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-02 18:44:38 +0100

    state

--- models/state.py
+++ models/state.py
@@ -6,7 +6,8 @@ import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from models.city import City
 from sqlalchemy.orm import relationship
-
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -14,15 +15,17 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all,delete", backref="state")
- 
-    @property     
-    def cities(self): 
-        """getter to list all cities""" 
-        from models import storage         
-        cities = storage.all(City)         
-        listofcities = []         
-        for city in cities:             
-            if city.state_id == self.id:
-                listofcities.append(city)         
-                
-        return listofcities
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+
+        @property
+        def cities(self):
+            """getter to list all cities"""
+            from models import storage
+            cities = models.storage.all(City).values()
+            listofcities = []
+            for city in cities:
+                if city.state_id == self.id:
+                    listofcities.append(city)
+
+            return listofcities
