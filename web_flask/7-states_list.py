632cf9b207264e8a4e11e83622a4bdba3e9a1382
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-02 20:32:24 +0100

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
 from models import *
@@ -7,16 +9,16 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """teardown handling"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
